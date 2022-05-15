e588a3c9a3bb1582a46610581860e6f3b1633bd9
shubham947222 <shubham947222@gmail.com>
2022-05-15 09:32:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./Pages/HomePage";
+import CoinPage from "./Pages/CoinPage";
+import { makeStyles } from "@material-ui/core";
 function App() {
+  const useStyles = makeStyles({
+    App: {
+      backgroundColor: "#14161a",
+      color: "white",
+      minHeight: "100vh",
+    },
+  });
+
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/coins/:id" element={<CoinPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/CoinInfo.js
@@ -0,0 +1,98 @@
+import {
+  CircularProgress,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+
+import { HistoricalChart } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+const CoinInfo = ({ coin }) => {
+  const [historicdata, setHistoricData] = useState();
+  const [days, useDays] = useState(1);
+
+  const { currency, symbol } = CryptoState();
+
+  const useStyles = makeStyles((theme) => ({
+    container: {
+      width: "75%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 25,
+      padding: 40,
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+        marginTop: 0,
+        padding: 20,
+        paddingTop: 0,
+      },
+    },
+  }));
+  const classes = useStyles();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+    setHistoricData(data.prices);
+  };
+
+  useEffect(() => {
+    fetchHistoricData();
+
+    // eslint-disable-next-line
+  }, [currency, days]);
+  console.log("historialdata->", historicdata);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <div className={classes.container}>
+        {!historicdata ? (
+          <CircularProgress
+            style={{ color: "gold" }}
+            size={250}
+            thickness={1}
+          />
+        ) : (
+          <>
+            <Line
+              data={{
+                labels: historicdata.map((coin) => {
+                  let date = new Date(coin[0]);
+                  let time =
+                    date.getHours() > 12
+                      ? `${date.getHours() - 12}:${date.getMinutes()} PM`
+                      : `${date.getHours()}:${date.getMinutes()} AM`;
+                  return days === 1 ? time : date.toLocaleDateString();
+                }),
+
+                datasets: [
+                  {
+                    data: historicdata.map((coin) => coin[1]),
+                    // label: `Price ( Past ${days} Days ) in ${currency}`,
+                    // borderColor: "#EEBC1D",
+                  },
+                ],
+              }}
+            />
+          </>
+        )}
+
+        {/* buttons */}
+      </div>
+    </ThemeProvider>
+  );
+};
+
+export default CoinInfo;
